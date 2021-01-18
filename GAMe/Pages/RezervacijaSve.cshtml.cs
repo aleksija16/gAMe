9d0e466d76bd4f739dd601cf41bdca618f54c7c7
aleksija16 <aleksanndra1616@outlook.com>
2021-01-18 01:04:11 +0100

    RezervacijaSve

--- GAMe/Pages/RezervacijaSve.cshtml.cs
+++ GAMe/Pages/RezervacijaSve.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cassandra;
+using GAMe.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,27 @@ namespace GAMe.Pages
 {
     public class RezervacijaSveModel : PageModel
     {
+        [BindProperty]
+        public IList<Rezervacija> SveRezervacije { get; set; }
         public void OnGet()
         {
+            ISession session = SessionManager.GetSession();
+            SveRezervacije = new List<Rezervacija>();
+
+            var sveRezervacije = session.Execute("select * from Rezervacija");
+
+            foreach (var rezervacijaSve in sveRezervacije)
+            {
+                Rezervacija rezervacija = new Rezervacija();
+                rezervacija.idRezervacija = (int)rezervacijaSve["idrezevacija"];
+                rezervacija.korisnikUsername = rezervacijaSve["korisnikusername"].ToString();
+                rezervacija.igraId = (int)rezervacijaSve["igraId"];
+                rezervacija.datum = (DateTime)rezervacijaSve["datum"];
+                rezervacija.trajanje =rezervacijaSve["trajanje"].ToString();
+
+                SveRezervacije.Add(rezervacija);
+            }
+
         }
     }
 }
